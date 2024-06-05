767fd66fe4210f82dd41e962e5a1163fe8fc1b82
ranieriprg <eddieranieri.prg@gmail.com>
2024-06-05 18:06:13 -0300

    criacao aplicacao banco nodejs

---
+++ index.js
@@ -0,0 +1,9 @@
+// importar modulos externos
+(async () => {
+  const inquirer = await import("inquirer");
+  const chalk = await import("chalk");
+  const fs = require("fs");
+
+  console.log("iniciamos o account");
+  
+})();
